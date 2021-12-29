0b427651e0f4e16b8e644ab696872d303fe9119f
SofijaMarcas <matthiasxywilliams@gmail.com>
2021-12-29 09:56:23 +0800

    Update helloworld.cpp, make helloworld.cpp more OO

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,24 +3,31 @@
 #include <string>
 
 using namespace std;
+class SofijaMarcas {
+    public:
+        void printHello();
+};
 
-int main()
-{
+void SofijaMarcas::printHello() {
     vector<string> msg = {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    for (const string& word : msg) {
+         cout << word << " ";
     }
     cout << endl;
-    
-    std::cout << "１人が意識不明か　兵庫・氷ノ山で男性５人遭難!" << std::endl;
+
+    std::cout << "25歳の元妻、資金が尽き「パパ活」をしていた" << std::endl;
 
     int i=0;
-    while((i++)<10)
-        cout<<"見てる？天気予報!\n";
+    while((i++)<3)
+        cout<<"若い身体を使って世渡りするのは構わないが殺人はまずいよ\n";
     cout<<endl;
+}
 
+int main(int argc, char** argv) {
+    SofijaMarcas sofija_marcas_temporary_;
+    sofija_marcas_temporary_.printHello();
+    cout<<endl;
     return 0;
     //aab4aa47eb3a0723f63427daed1a7bcfca5b6e5b
 }
